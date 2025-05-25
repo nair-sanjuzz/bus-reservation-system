bef7729f3b6575eac86b63078fbb9098f7a9775d
Sanjay <sanjaynair2003@gmail.com>
2025-05-25 12:09:04 +0530

    Updated navbar.js

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,8 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import logo from './bus-logo.png';
 import '../styles/main.scss';
 
-function Navbar() {
-    const navigate = useNavigate();k
+function Navbar({ isLoggedIn, setIsLoggedIn }) {
+    const navigate = useNavigate();
 
     const handleLogout = () => {
         sessionStorage.removeItem("admin");
